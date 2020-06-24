ab411bca0d86d252aaa15b8a537171de4d79a7b5
Jakub <jakub.jakubecky123@gmail.com>
2020-06-25 00:45:57 +0200

    God only knows what I did here bruh

--- src/main/java/jrazek/pong/learning/SelfLearningModule.java
+++ src/main/java/jrazek/pong/learning/SelfLearningModule.java
@@ -32,7 +32,8 @@ public class SelfLearningModule {
                 sum += var * indexes.get(i);
             else
                 sum += var * indexes.get(Utils.randomInt(0, indexes.size()-1));
-            i++;
+            i++;//
+
         }
         //random index from list. Must use all of them!!
         paddle.setVelocity(new Utils.Vector2I((int)sum, 0));
